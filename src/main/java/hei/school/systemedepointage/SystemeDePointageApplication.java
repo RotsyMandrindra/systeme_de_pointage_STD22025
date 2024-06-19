88c854c03b82f514e4fef798ed9337178fbaf418
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2024-06-19 10:06:07 +0300

    chore: generate an empty springboot maven project

---
+++ src/main/java/hei/school/systemedepointage/SystemeDePointageApplication.java
@@ -0,0 +1,13 @@
+package hei.school.systemedepointage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeDePointageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeDePointageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hei/school/systemedepointage/SystemeDePointageApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.systemedepointage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeDePointageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
